ff627cc91e6f5af1fd982249e411b407645e046b
Daniel Drews <daniel.drews@gmail.com>
2020-04-15 20:27:12 -0300

    basic components (square, board, game)

---
+++ src/square/square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './square.css';
+
+export default class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+}
+  
